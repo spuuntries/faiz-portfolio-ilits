29e74384bb678027c395fb52f80b6e33c22c12b3
spuuntries <kek@spuun.art>
2023-10-06 11:28:54 +0700

    feat: Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const procenv = process.env,
+  express = require("express"),
+  app = express(),
+  fs = require("fs"),
+  slowDown = require("express-slow-down");
+
+app.enable("trust proxy");
+app.use(morgan("combined"));
+
+const mainSlowDown = slowDown({
+  windowMs: 15 * 60 * 1000,
+  delayAfter: 10,
+  delayMs: 500,
+});
+app.use("/", mainSlowDown, express.static("static"));
+app.use("/assets", express.static("static"));
+
+app.get("/projects", (req, res) => {
+  res.json(JSON.parse(fs.readFileSync("projects.json").toString()));
+});
+
+app.all("*", (req, res) => {
+  res.sendStatus(404);
+});
+
+app.listen(procenv.PORT, () => {
+  console.log(`${new Date()}: listening on port ${procenv.PORT}`);
+});
